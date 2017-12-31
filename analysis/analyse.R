a6e6c6635353142343a66b0c600279135302652d
Lucas Cherkewski <lucas@ecustom.ca>
2017-12-31 11:35:55 -0500

    add output step

--- analysis/analyse.R
+++ analysis/analyse.R
@@ -1,8 +1,14 @@
 library(corpus)
 
+# load the text
 aogg_text <- paste(readLines("../aogg.txt"), collapse = "\n")
 
+# extract the questions
 sentences <- text_split(aogg_text, "sentences")
 sentences$text <- gsub('\n', ' ', sentences$text) # replace newlines with spaces
 
+# extract just sentences with question marks in them
 questions <- sentences[grep("?", sentences$text, fixed = TRUE),]
+
+# output the questions to a CSV
+write.csv(questions, "aogg_questions.csv")
